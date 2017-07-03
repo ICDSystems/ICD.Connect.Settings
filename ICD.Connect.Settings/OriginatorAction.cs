dc2068fc002e551bfcc9c4dfa16917f1ae7be917
jeff.thompson <jeffery.thompson@wedoresi.com>
2017-07-03 09:58:07 -0400

    OriginatorAction for permissions

---
+++ ICD.Connect.Settings/OriginatorAction.cs
@@ -0,0 +1,15 @@
+﻿using ICD.Common.Permissions;
+
+namespace ICD.Connect.Settings
+{
+	public class OriginatorAction : Action
+	{
+		protected OriginatorAction(string value) : base(value)
+		{
+		}
+
+		public IAction CopySettings { get { return new OriginatorAction("IOriginator.CopySettings"); } }
+		public IAction ApplySettings { get { return new OriginatorAction("IOriginator.ApplySettings"); } }
+		public IAction ClearSettings { get { return new OriginatorAction("IOriginator.ClearSettings"); } }
+	}
+}
