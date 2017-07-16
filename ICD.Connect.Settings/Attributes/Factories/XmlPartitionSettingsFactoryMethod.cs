caf3b8766d7e051ed99f19efc694a9de8ff57b68
Chris Cameron <chris.cameron@wedoresi.com>
2017-07-16 13:22:19 -0400

    Initial commit of XmlPartitionSettingsFactoryMethod

---
+++ ICD.Connect.Settings/Attributes/Factories/XmlPartitionSettingsFactoryMethod.cs
@@ -0,0 +1,14 @@
+﻿namespace ICD.Connect.Settings.Attributes.Factories
+{
+	public sealed class XmlPartitionSettingsFactoryMethod : AbstractXmlFactoryMethodAttribute
+	{
+		/// <summary>
+		/// Constructor.
+		/// </summary>
+		/// <param name="typeName"></param>
+		public XmlPartitionSettingsFactoryMethod(string typeName)
+			: base(typeName)
+		{
+		}
+	}
+}
