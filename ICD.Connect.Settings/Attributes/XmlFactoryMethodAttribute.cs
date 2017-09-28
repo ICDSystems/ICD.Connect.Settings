9688ef91f63f3ed7825ebe1c97446cc32b1d1190
Chris Cameron <chris.cameron@wedoresi.com>
2017-09-28 12:25:56 -0400

    Reducing factory attributes to a single type

---
+++ ICD.Connect.Settings/Attributes/XmlFactoryMethodAttribute.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using ICD.Common.Attributes;
+using ICD.Common.Properties;
+
+namespace ICD.Connect.Settings.Attributes
+{
+	[MeansImplicitUse]
+	[AttributeUsage(AttributeTargets.Method)]
+	public sealed class XmlFactoryMethodAttribute : AbstractIcdAttribute
+	{
+		private readonly string m_FactoryName;
+
+		/// <summary>
+		/// Returns the typename.
+		/// </summary>
+		[PublicAPI]
+		public string FactoryName { get { return m_FactoryName; } }
+
+		#region Constructors
+
+		/// <summary>
+		/// Constructor.
+		/// </summary>
+		/// <param name="factoryName"></param>
+		public XmlFactoryMethodAttribute(string factoryName)
+		{
+			m_FactoryName = factoryName;
+		}
+
+		#endregion
+	}
+}
