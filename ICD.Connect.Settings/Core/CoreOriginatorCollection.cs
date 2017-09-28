c5fc92e1502598674444dc31e59416b359d9b96b
Chris Cameron <chris.cameron@wedoresi.com>
2017-09-28 16:54:27 -0400

    Tidying

--- ICD.Connect.Settings/Core/CoreOriginatorCollection.cs
+++ ICD.Connect.Settings/Core/CoreOriginatorCollection.cs
@@ -1,18 +1,25 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace ICD.Connect.Settings.Core
 {
 	public sealed class CoreOriginatorCollection : AbstractOriginatorCollection<IOriginator>
 	{
+		/// <summary>
+		/// Constructor.
+		/// </summary>
 		public CoreOriginatorCollection()
+			: this(Enumerable.Empty<IOriginator>())
 		{
-			
 		}
 
+		/// <summary>
+		/// Constructor.
+		/// </summary>
+		/// <param name="children"></param>
 		public CoreOriginatorCollection(IEnumerable<IOriginator> children)
 			: base(children)
 		{
-			
 		}
 	}
 }
