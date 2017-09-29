3d40e5677c973d8e7498ecac337ecb6c3080893c
Chris Cameron <chris.cameron@wedoresi.com>
2017-09-29 10:41:57 -0400

    Updating AssemblyInfo

--- ICD.Connect.Settings/Properties/AssemblyInfo.cs
+++ ICD.Connect.Settings/Properties/AssemblyInfo.cs
@@ -1,7 +1,7 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyTitle("ICD.Connect.Settings")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Connect.Settings")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright © ICD Systems 2017")]
 [assembly: AssemblyVersion("1.0.0.*")]
