a69ba1c9a77be592c178c93393a450e75cb11f19
Chris Cameron <chris.cameron@wedoresi.com>
2017-11-09 10:51:02 -0500

    Initial commit of IdUtilsTest

---
+++ ICD.Connect.Settings.Tests/IdUtilsTest.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ICD.Connect.Settings.Tests
+{
+	[TestFixture]
+    public sealed class IdUtilsTest
+    {
+		[Test]
+		public void GetNewIdTest()
+		{
+			IEnumerable<int> ids = Enumerable.Range(1, 10);
+			Assert.AreEqual(11, IdUtils.GetNewId(ids));
+		}
+	}
+}
