4b6039ab6c6983dd3de66d3b0b426066ccc101f8
Chris Cameron <chris.cameron@wedoresi.com>
2017-11-12 08:59:48 -0500

    Initial commit of XmlFactoryMethodAttributeTest

---
+++ ICD.Connect.Settings.Tests/Attributes/XmlFactoryMethodAttributeTest.cs
@@ -0,0 +1,16 @@
+﻿using ICD.Common.Utils.Tests.Attributes;
+using ICD.Connect.Settings.Attributes;
+using NUnit.Framework;
+
+namespace ICD.Connect.Settings.Tests.Attributes
+{
+	[TestFixture]
+    public sealed class XmlFactoryMethodAttributeTest : AbstractIcdAttributeTest
+    {
+		[TestCase("Test")]
+		public void FactoryNameTest(string name)
+		{
+			Assert.AreEqual(name, new XmlFactoryMethodAttribute(name).FactoryName);
+		}
+    }
+}
