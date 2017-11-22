e59906cbb8f1f772e87f97d94f5891944d580648
Chris Cameron <chris.cameron@wedoresi.com>
2017-11-22 10:10:14 -0500

    Renaming Action to Permissable

---
+++ ICD.Connect.Settings/OriginatorPermissable.cs
@@ -0,0 +1,15 @@
+﻿using ICD.Common.Permissions;
+
+namespace ICD.Connect.Settings
+{
+	public class OriginatorPermissable : Permissable
+	{
+		protected OriginatorPermissable(string name) : base(name)
+		{
+		}
+
+		public IPermissable CopySettings { get { return new OriginatorPermissable("IOriginator.CopySettings"); } }
+		public IPermissable ApplySettings { get { return new OriginatorPermissable("IOriginator.ApplySettings"); } }
+		public IPermissable ClearSettings { get { return new OriginatorPermissable("IOriginator.ClearSettings"); } }
+	}
+}
