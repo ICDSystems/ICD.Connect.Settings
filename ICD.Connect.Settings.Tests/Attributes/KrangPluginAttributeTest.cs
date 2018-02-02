ef62dd8b66add7dcda29de727fb141b4244169c0
Chris Cameron <chris.cameron@wedoresi.com>
2018-02-02 14:41:36 -0500

    Test stub

---
+++ ICD.Connect.Settings.Tests/Attributes/KrangPluginAttributeTest.cs
@@ -0,0 +1,10 @@
+﻿using ICD.Common.Utils.Tests.Attributes;
+using NUnit.Framework;
+
+namespace ICD.Connect.Settings.Tests.Attributes
+{
+	[TestFixture]
+	public sealed class KrangPluginAttributeTest : AbstractIcdAttributeTest
+	{
+	}
+}
