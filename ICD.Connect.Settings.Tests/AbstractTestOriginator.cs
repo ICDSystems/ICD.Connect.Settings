719efdfc6f81bc0b7a8a1fd10c3052079d4fbac9
Chris Cameron <chris.cameron@wedoresi.com>
2018-03-02 10:44:40 -0500

    Initial commit of TestOriginator

---
+++ ICD.Connect.Settings.Tests/AbstractTestOriginator.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using ICD.Common.Permissions;
+using ICD.Common.Utils.Services.Logging;
+using ICD.Connect.Settings.Core;
+
+namespace ICD.Connect.Settings.Tests
+{
+	public abstract class AbstractTestOriginator<TSettings> : IOriginator<TSettings>
+		where TSettings : ISettings
+	{
+		public event EventHandler OnSettingsClearing;
+		public event EventHandler OnSettingsCleared;
+		public event EventHandler OnSettingsApplied;
+
+		public int Id { get; set; }
+
+		public string Name { get; set; }
+
+		public string CombineName { get; set; }
+
+		public IEnumerable<Permission> Permissions { get { throw new NotImplementedException(); } }
+
+		public bool Serialize { get; set; }
+
+		public ILoggerService Logger { get; set; }
+
+		public ISettings CopySettings()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void CopySettings(TSettings settings)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void ApplySettings(TSettings settings, IDeviceFactory factory)
+		{
+			throw new NotImplementedException();
+		}
+
+		TSettings IOriginator<TSettings>.CopySettings()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void CopySettings(ISettings settings)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void ApplySettings(ISettings settings, IDeviceFactory factory)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void ClearSettings()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
