f7bce7c49be0160f8e7c3b03834d7e8c09219ce2
Chris Cameron <chris.cameron@wedoresi.com>
2018-03-26 11:47:55 -0400

    Initial commit of OriginatorApi

---
+++ ICD.Connect.Settings/OriginatorApi.cs
@@ -0,0 +1,13 @@
+﻿namespace ICD.Connect.Settings
+{
+	public static class OriginatorApi
+	{
+		public const string PROPERTY_ID = "Id";
+		public const string PROPERTY_NAME = "Name";
+		public const string PROPERTY_COMBINE_NAME = "CombineName";
+
+		public const string HELP_PROPERTY_ID = "The unique ID for this originator.";
+		public const string HELP_PROPERTY_NAME = "The human readable name for this originator.";
+		public const string HELP_PROPERTY_COMBINE_NAME = "The human readable name for this originator in a combine space.";
+	}
+}
