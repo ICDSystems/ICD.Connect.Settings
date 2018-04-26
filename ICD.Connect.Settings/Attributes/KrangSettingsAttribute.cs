183faad8d1328ec2d5b83560e2cc9fe08452bf68
Chris Cameron <chris.cameron@wedoresi.com>
2018-04-26 10:26:59 -0400

    feat: KrangSettings factory name and originator type now inferred via reflection

--- ICD.Connect.Settings/Attributes/KrangSettingsAttribute.cs
+++ ICD.Connect.Settings/Attributes/KrangSettingsAttribute.cs
@@ -9,20 +9,39 @@ namespace ICD.Connect.Settings.Attributes
 	public sealed class KrangSettingsAttribute : AbstractIcdAttribute
 	{
 		private readonly string m_FactoryName;
+		private readonly Type m_OriginatorType;
 
 		/// <summary>
-		/// Returns the typename.
+		/// Returns the registered factory name.
 		/// </summary>
 		[PublicAPI]
 		public string FactoryName { get { return m_FactoryName; } }
 
 		/// <summary>
+		/// Returns the type of originator to instantiate.
+		/// </summary>
+		[PublicAPI]
+		public Type OriginatorType { get { return m_OriginatorType; } }
+
+		/// <summary>
 		/// Constructor.
 		/// </summary>
 		/// <param name="factoryName"></param>
+		[Obsolete("Provide originator type to constructor")]
 		public KrangSettingsAttribute(string factoryName)
+			: this(factoryName, null)
+		{
+		}
+
+		/// <summary>
+		/// Constructor.
+		/// </summary>
+		/// <param name="factoryName"></param>
+		/// <param name="originatorType"></param>
+		public KrangSettingsAttribute(string factoryName, Type originatorType)
 		{
 			m_FactoryName = factoryName;
+			m_OriginatorType = originatorType;
 		}
 	}
 }
