dbf69e8504dfbfe758bc1cdf661d8eb20abc04fe
Chris Cameron <chris.cameron@wedoresi.com>
2018-05-07 14:31:41 -0400

    refactor: Resolving warnings

--- ICD.Connect.Settings/Core/CoreSettings.cs
+++ ICD.Connect.Settings/Core/CoreSettings.cs
@@ -1,18 +1,11 @@
-﻿using System;
-using ICD.Connect.Settings.Attributes;
+﻿using ICD.Connect.Settings.Attributes;
 
 namespace ICD.Connect.Settings.Core
 {
-	[KrangSettings(FACTORY_NAME)]
+	[KrangSettings("Core", typeof(Core))]
 	public sealed class CoreSettings : AbstractCoreSettings
 	{
-		private const string FACTORY_NAME = "Core";
-
 		private readonly SettingsCollection m_OriginatorSettings;
-
-		public override string FactoryName { get { return FACTORY_NAME; } }
-
-		public override Type OriginatorType { get { return typeof(Core); } }
 
 		public override SettingsCollection OriginatorSettings { get { return m_OriginatorSettings; } }
 
