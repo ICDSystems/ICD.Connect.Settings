efb6d90bce90c5fae3b209b9b03a082b15fe2319
Chris Cameron <chris.cameron@wedoresi.com>
2018-05-11 11:59:01 -0400

    feat: Initial commit of IUsernamePasswordSettings

---
+++ ICD.Connect.Settings/Settings/IUsernamePasswordSettings.cs
@@ -0,0 +1,15 @@
+﻿namespace ICD.Connect.Settings
+{
+	public interface IUsernamePasswordSettings : ISettings
+	{
+		/// <summary>
+		/// Gets/sets the configurable username.
+		/// </summary>
+		string Username { get; set; }
+
+		/// <summary>
+		/// Gets/sets the configurable password.
+		/// </summary>
+		string Password { get; set; }
+	}
+}
