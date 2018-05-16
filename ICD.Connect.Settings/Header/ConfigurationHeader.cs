3cd873563ffcc0d20c0a1b30d1188c895d9084ba
Chris Cameron <chris.cameron@wedoresi.com>
2018-05-16 15:49:36 -0400

    chore: Incrementing config version number

--- ICD.Connect.Settings/Header/ConfigurationHeader.cs
+++ ICD.Connect.Settings/Header/ConfigurationHeader.cs
@@ -7,7 +7,7 @@ namespace ICD.Connect.Settings.Header
 {
 	public sealed class ConfigurationHeader
 	{
-		private static readonly Version s_CurrentConfigVersion = new Version("2.0");
+		private static readonly Version s_CurrentConfigVersion = new Version("3.0");
 
 		public const string HEADER_ELEMENT = "Header";
 		private const string CONFIG_VERSION_ELEMENT = "ConfigVersion";
