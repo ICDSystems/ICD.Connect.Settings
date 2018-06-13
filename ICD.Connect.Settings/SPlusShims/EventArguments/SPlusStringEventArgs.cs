6ca70367bb1fa4130d4322ead4dbfc751e20f049
Jack Kanarish <jack.kanarish@wedoresi.com>
2018-06-13 17:06:52 -0400

    feat: create splus specific event types

---
+++ ICD.Connect.Settings/SPlusShims/EventArguments/SPlusStringEventArgs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using ICD.Common.Properties;
+
+namespace ICD.Connect.Settings.SPlusShims.EventArguments
+{
+	public sealed class SPlusStringEventArgs : EventArgs
+	{
+		[PublicAPI("S+")]
+		public string Data { get; set; }
+
+		[PublicAPI("S+")]
+		public SPlusStringEventArgs(){}
+
+		public SPlusStringEventArgs(string data)
+		{
+			Data = data;
+		}
+	}
+
+}
---
+++ ICD.Connect.Settings/SPlusShims/EventArguments/SPlusUshortEventArgs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using ICD.Common.Properties;
+
+namespace ICD.Connect.Settings.SPlusShims.EventArguments
+{
+	public sealed class SPlusUshortEventArgs : EventArgs
+	{
+		[PublicAPI("S+")]
+		public ushort Data { get; set; }
+
+		[PublicAPI("S+")]
+		public SPlusUshortEventArgs(){}
+
+		public SPlusUshortEventArgs(ushort data)
+		{
+			Data = data;
+		}
+	}
+}
