c34ca6961f52ac4abee45e37cd0b46386ebb7393
Chris Cameron <chris.cameron@wedoresi.com>
2019-05-24 10:27:26 -0400

    chore: Updating changelog, incrementing patch version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,7 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 
 ## [Unreleased]
 
+## [7.4.1] - 2019-05-24
 ### Added
  - Added CardAddressSettingsPropertyAttribute
  - Added CardParentSettingsPropertyAttribute
--- ICD.Connect.Settings/Properties/AssemblyInfo.cs
+++ ICD.Connect.Settings/Properties/AssemblyInfo.cs
@@ -4,4 +4,4 @@ using System.Reflection;
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Connect.Settings")]
 [assembly: AssemblyCopyright("Copyright © ICD Systems 2019")]
-[assembly: AssemblyVersion("7.4.0.0")]
+[assembly: AssemblyVersion("7.4.1.0")]
