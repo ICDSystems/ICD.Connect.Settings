a5e797b948a2e8e445c4331b106b1cef2b370f1e
Chris Cameron <chris.cameron@icdsystems.com>
2020-03-20 14:05:24 -0400

    chore: Updating changelog, incrementing major version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,7 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 
 ## [Unreleased]
 
+## [10.0.0] - 2020-03-20
 ### Added
  - Added validation methods to settings
  - Permissions are serialized back to XML
--- ICD.Connect.Settings/Properties/AssemblyInfo.cs
+++ ICD.Connect.Settings/Properties/AssemblyInfo.cs
@@ -4,4 +4,4 @@ using System.Reflection;
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Connect.Settings")]
 [assembly: AssemblyCopyright("Copyright © ICD Systems 2020")]
-[assembly: AssemblyVersion("9.3.0.0")]
+[assembly: AssemblyVersion("10.0.0.0")]
