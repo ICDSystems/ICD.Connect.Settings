c49b5562f9e13b3f863efd4835cbe7fa28737fd5
Chris Cameron <chris.cameron@wedoresi.com>
2020-06-29 12:03:32 -0400

    fix: Excluding obfuscation on SQLite ColumnInfo class and members

--- ICD.Connect.Settings/ORM/Databases/SqliteDatabase.cs
+++ ICD.Connect.Settings/ORM/Databases/SqliteDatabase.cs
@@ -1,3 +1,4 @@
+using ICD.Common.Properties;
 #if SIMPLSHARP
 using Crestron.SimplSharp.CrestronData;
 using Crestron.SimplSharp.SQLite;
@@ -89,6 +90,7 @@ namespace ICD.Connect.Settings.ORM.Databases
 			return name == GetConnection().ExecuteScalar(sql, null, transaction) as string;
 		}
 
+		[Obfuscation(Exclude = true, ApplyToMembers = true)]
 		// public due to Eazfuscator issue related to properties on a nested private class
 		public sealed class SqliteColumnInfo
 		{
