9d118dbb4f743e45cecf0fa991a3e029df5673e3
Chris Cameron <chris.cameron@icdsystems.com>
2020-07-16 16:06:53 -0400

    refactor: Tidying, potential thread safety

--- ICD.Connect.Settings/SettingsCollection.cs
+++ ICD.Connect.Settings/SettingsCollection.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using ICD.Common.Properties;
 using ICD.Common.Utils;
 using ICD.Common.Utils.Collections;
 using ICD.Common.Utils.EventArguments;
@@ -46,8 +47,11 @@ namespace ICD.Connect.Settings
 		/// Constructor.
 		/// </summary>
 		/// <param name="settings"></param>
-		public SettingsCollection(IEnumerable<ISettings> settings)
+		public SettingsCollection([NotNull] IEnumerable<ISettings> settings)
 		{
+			if (settings == null)
+				throw new ArgumentNullException("settings");
+
 			m_Collection = new IcdOrderedDictionary<int, ISettings>();
 			m_CollectionSection = new SafeCriticalSection();
 
@@ -64,8 +68,11 @@ namespace ICD.Connect.Settings
 		/// <param name="writer"></param>
 		/// <param name="element"></param>
 		/// <param name="childElement"></param>
-		public void ToXml(IcdXmlTextWriter writer, string element, string childElement)
+		public void ToXml([NotNull] IcdXmlTextWriter writer, string element, string childElement)
 		{
+			if (writer == null)
+				throw new ArgumentNullException("writer");
+
 			m_CollectionSection.Enter();
 
 			try
@@ -96,7 +103,7 @@ namespace ICD.Connect.Settings
 		/// Adds the item to the collection unless the id already exists
 		/// </summary>
 		/// <param name="item"></param>
-		public bool Add(ISettings item)
+		public bool Add([NotNull] ISettings item)
 		{
 			if (item == null)
 				throw new ArgumentNullException("item");
@@ -123,38 +130,26 @@ namespace ICD.Connect.Settings
 		/// Adds the items to the collection.
 		/// </summary>
 		/// <param name="items"></param>
-		public void AddRange(IEnumerable<ISettings> items)
+		public void AddRange([NotNull] IEnumerable<ISettings> items)
 		{
-			m_CollectionSection.Enter();
+			if (items == null)
+				throw new ArgumentNullException("items");
 
-			try
-			{
-				foreach (ISettings item in items)
-					Add(item);
-			}
-			finally
-			{
-				m_CollectionSection.Leave();
-			}
+			foreach (ISettings item in items)
+				Add(item);
 		}
 
 		/// <summary>
 		/// Clears the collection and sets the items.
 		/// </summary>
 		/// <param name="items"></param>
-		public void SetRange(IEnumerable<ISettings> items)
+		public void SetRange([NotNull] IEnumerable<ISettings> items)
 		{
-			m_CollectionSection.Enter();
+			if (items == null)
+				throw new ArgumentNullException("items");
 
-			try
-			{
-				Clear();
-				AddRange(items);
-			}
-			finally
-			{
-				m_CollectionSection.Leave();
-			}
+			Clear();
+			AddRange(items);
 		}
 
 		/// <summary>
@@ -162,6 +157,7 @@ namespace ICD.Connect.Settings
 		/// </summary>
 		/// <param name="id"></param>
 		/// <returns></returns>
+		[NotNull]
 		public ISettings GetById(int id)
 		{
 			m_CollectionSection.Enter();
@@ -186,17 +182,8 @@ namespace ICD.Connect.Settings
 		/// </summary>
 		public void Clear()
 		{
-			m_CollectionSection.Enter();
-
-			try
-			{
-				foreach (ISettings item in m_Collection.Values.ToArray(m_Collection.Count))
-					Remove(item);
-			}
-			finally
-			{
-				m_CollectionSection.Leave();
-			}
+			foreach (ISettings item in m_CollectionSection.Execute(() => m_Collection.Values.ToArray(m_Collection.Count)))
+				Remove(item);
 		}
 
 		/// <summary>
@@ -204,8 +191,11 @@ namespace ICD.Connect.Settings
 		/// </summary>
 		/// <param name="item"></param>
 		/// <returns></returns>
-		public bool Contains(ISettings item)
+		public bool Contains([NotNull] ISettings item)
 		{
+			if (item == null)
+				throw new ArgumentNullException("item");
+
 			return ContainsId(item.Id);
 		}
 
@@ -224,8 +214,11 @@ namespace ICD.Connect.Settings
 		/// </summary>
 		/// <param name="array"></param>
 		/// <param name="arrayIndex"></param>
-		public void CopyTo(ISettings[] array, int arrayIndex)
+		public void CopyTo([NotNull] ISettings[] array, int arrayIndex)
 		{
+			if (array == null)
+				throw new ArgumentNullException("array");
+
 			m_CollectionSection.Enter();
 
 			try
@@ -245,8 +238,11 @@ namespace ICD.Connect.Settings
 		/// </summary>
 		/// <param name="item"></param>
 		/// <returns></returns>
-		public bool Remove(ISettings item)
+		public bool Remove([NotNull] ISettings item)
 		{
+			if (item == null)
+				throw new ArgumentNullException("item");
+
 			m_CollectionSection.Enter();
 
 			try
