26dd7b60c9d38090852e6d26cceba5c1c672b029
Chris Cameron <chris.cameron@wedoresi.com>
2021-02-26 13:53:25 -0500

    feat: Exposing ICore features in the console

--- ICD.Connect.Settings/Cores/AbstractCore.cs
+++ ICD.Connect.Settings/Cores/AbstractCore.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Collections.Generic;
+using ICD.Connect.API.Commands;
+using ICD.Connect.API.Nodes;
 using ICD.Connect.Settings.Organizations;
 using ICD.Connect.Settings.Originators;
 
@@ -128,6 +131,65 @@ namespace ICD.Connect.Settings.Cores
 		public void LoadSettings(Action postApplyAction)
 		{
 			FileOperations.LoadCoreSettings<AbstractCore<TSettings>, TSettings>(this, postApplyAction);
+		}
+
+		#endregion
+
+		#region Console
+
+		/// <summary>
+		/// Gets the child console nodes.
+		/// </summary>
+		/// <returns></returns>
+		public override IEnumerable<IConsoleNodeBase> GetConsoleNodes()
+		{
+			foreach (IConsoleNodeBase node in GetBaseConsoleNodes())
+				yield return node;
+
+			foreach (IConsoleNodeBase node in CoreConsole.GetConsoleNodes(this))
+				yield return node;
+		}
+
+		/// <summary>
+		/// Workaround for "unverifiable code" warning.
+		/// </summary>
+		/// <returns></returns>
+		private IEnumerable<IConsoleNodeBase> GetBaseConsoleNodes()
+		{
+			return base.GetConsoleNodes();
+		}
+
+		/// <summary>
+		/// Calls the delegate for each console status item.
+		/// </summary>
+		/// <param name="addRow"></param>
+		public override void BuildConsoleStatus(AddStatusRowDelegate addRow)
+		{
+			base.BuildConsoleStatus(addRow);
+
+			CoreConsole.BuildConsoleStatus(this, addRow);
+		}
+
+		/// <summary>
+		/// Gets the child console commands.
+		/// </summary>
+		/// <returns></returns>
+		public override IEnumerable<IConsoleCommand> GetConsoleCommands()
+		{
+			foreach (IConsoleCommand command in GetBaseConsoleCommands())
+				yield return command;
+
+			foreach (IConsoleCommand command in CoreConsole.GetConsoleCommands(this))
+				yield return command;
+		}
+
+		/// <summary>
+		/// Workaround for "unverifiable code" warning.
+		/// </summary>
+		/// <returns></returns>
+		private IEnumerable<IConsoleCommand> GetBaseConsoleCommands()
+		{
+			return base.GetConsoleCommands();
 		}
 
 		#endregion
---
+++ ICD.Connect.Settings/Cores/CoreConsole.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using ICD.Connect.API.Commands;
+using ICD.Connect.API.Nodes;
+
+namespace ICD.Connect.Settings.Cores
+{
+	public static class CoreConsole
+	{
+		/// <summary>
+		/// Gets the child console nodes.
+		/// </summary>
+		/// <param name="instance"></param>
+		/// <returns></returns>
+		public static IEnumerable<IConsoleNodeBase> GetConsoleNodes(ICore instance)
+		{
+			if (instance == null)
+				throw new ArgumentNullException("instance");
+
+			yield break;
+		}
+
+		/// <summary>
+		/// Calls the delegate for each console status item.
+		/// </summary>
+		/// <param name="instance"></param>
+		/// <param name="addRow"></param>
+		public static void BuildConsoleStatus(ICore instance, AddStatusRowDelegate addRow)
+		{
+			if (instance == null)
+				throw new ArgumentNullException("instance");
+
+			if (addRow == null)
+				throw new ArgumentNullException("addRow");
+
+			string orgString =
+				instance.Organization.Id == 0
+					? "Unknown"
+					: string.Format("{0} - {1}", instance.Organization.Id, instance.Organization.Name);
+
+			addRow("Organization", orgString);
+			addRow("Culture", instance.Localization.CurrentCulture.Name);
+			addRow("Culture (UI)", instance.Localization.CurrentUiCulture.Name);
+			addRow("Core Start Time", instance.CoreStartTime);
+		}
+
+		/// <summary>
+		/// Gets the child console commands.
+		/// </summary>
+		/// <param name="instance"></param>
+		/// <returns></returns>
+		public static IEnumerable<IConsoleCommand> GetConsoleCommands(ICore instance)
+		{
+			if (instance == null)
+				throw new ArgumentNullException("instance");
+
+			yield break;
+		}
+	}
+}
