ffcddd81cffa1b31eeb4f4ff78f16243b522fe24
Drew Tingen <drew.tingen@icdsystems.com>
2023-03-22 08:18:35 -0400

    chore: Update copyright year

--- ICD.Connect.Settings/Properties/AssemblyInfo.cs
+++ ICD.Connect.Settings/Properties/AssemblyInfo.cs
@@ -3,5 +3,5 @@ using System.Reflection;
 [assembly: AssemblyTitle("ICD.Connect.Settings")]
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Connect.Settings")]
-[assembly: AssemblyCopyright("Copyright © ICD Systems 2022")]
+[assembly: AssemblyCopyright("Copyright © ICD Systems 2023")]
 [assembly: AssemblyVersion("12.1.3.0")]
